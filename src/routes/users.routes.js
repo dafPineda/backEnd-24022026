60e0f4e8d37cc0171bbf9682820019c099d698f2
Dafne Pineda <152250662+dafPineda@users.noreply.github.com>
2026-02-25 14:54:49 -0600

    Delete src directory

--- src/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const controller = require('../controllers/users.controller.js');
-const {authMiddleware, requireRole} = require('../auth.js')
-
-router.post('/login',controller.logInUser)
-router.post('/create', controller.create)
-
-module.exports = { router };
